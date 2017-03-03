e79f5856da3ac741b4aa307b183be5d95ba28530
HJR <HJR@DESKTOP-41I1JEU>
2017-03-03 15:10:28 -0600

    Add project files.

---
+++ EscuelaConsola/Program.cs
@@ -0,0 +1,67 @@
+﻿using EscuelaLibreria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscuelaConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var alumnos = generarAlumnos();
+            List<Alumno>alumnosAprobados = new List<Alumno>();
+            List<Alumno>alumnosNoAprobados = new List<Alumno>();
+            foreach (var alumno in alumnos)
+            {
+                if(alumno.Promedio<6)
+                    alumnosNoAprobados.Add(alumno);
+                else alumnosAprobados.Add(alumno);
+
+                
+            }
+
+            Console.WriteLine($"Hubo {alumnosAprobados.Count} aprobados");
+
+            foreach (var alumno in alumnosAprobados)
+              
+            {
+                Console.WriteLine($"Nombre: {alumno.Nombre} Promedio: {alumno.Promedio}");
+            }
+            Console.WriteLine($"Hubo {alumnosNoAprobados.Count} no aprobados");
+            foreach (var alumno in alumnosNoAprobados)
+            {
+                Console.WriteLine($"Nombre: {alumno.Nombre} Promedio: {alumno.Promedio}");
+            }
+                   
+            Console.ReadKey();
+
+        }
+
+        private static List<Alumno> generarAlumnos()
+        {
+            List<Alumno> alumnos = new List<Alumno>();
+            for (int i = 0; i < 10; i++)
+            {
+                Random rnd = new Random(DateTime.Now.Millisecond);
+                var materias = new List<Materia>();
+                for (int j = 0; j < rnd.Next(1,5); j++)
+                {
+                    materias.Add(new Materia((Materias)rnd.Next(0,6), rnd.NextDouble() * 10));
+                }
+                var alumno = new Alumno(i, "Alumno " + i);
+                alumno.AsignarMaterias(materias);
+
+                alumnos.Add(alumno);
+                Task.Delay(100).Wait();
+            }
+            return alumnos;
+        }
+
+       
+
+    }
+}
---
+++ EscuelaLibreria/Entidades/Alumno.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscuelaLibreria
+{
+    public enum Materias
+    {
+        Español,
+        Matematicas,
+        Ciencias,
+        Fisica,
+        Ingles,
+        Programacion
+    }
+    public class Alumno
+    {
+        private int matricula;
+
+        public int Matricula
+        {
+            get { return matricula; }
+        }
+
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        private string nivelAcademico;
+
+        public string NivelAcademico
+        {
+            get { return nivelAcademico; }
+            set { nivelAcademico = value; }
+        }
+
+        private double promedio;
+
+        public double Promedio
+        {
+            get { return promedio; }
+        }
+
+        private List<Materia> materias;
+
+        public List<Materia> Materias
+        {
+            get { return materias; }
+        }
+
+        public Alumno(int matricula, string nombre)
+        {
+            this.matricula = matricula;
+            this.nombre = nombre;
+            this.materias = new List<Materia>();
+        }
+
+        public bool AsignarMaterias(List<Materia> materias)
+        {
+            if (materias != null & materias.Count() >= 2 && materias.Count() <= 4)
+            {
+                this.materias = materias;
+                PromedioAlumno();
+                return true;
+            }
+            return false;
+
+        }
+
+        private void PromedioAlumno()
+        {
+            double suma = 0;
+            for (int i = 0; i < materias.Count; i++)
+            {
+                suma += materias.ElementAt(i).Calificacion;
+            }
+            promedio = suma / materias.Count;
+            
+        }
+        
+
+    }
+
+    public class Materia
+    {
+        private Materias nombre;
+
+        public Materias Nombre
+        {
+            get { return nombre; }
+        
+        }
+        private double calificacion; 
+
+        public double Calificacion
+        {
+            get { return calificacion; }
+           
+        }
+
+        public Materia(Materias materia,double calificacion)
+        {
+            this.nombre = materia;
+            this.calificacion = calificacion;
+        }
+
+    }
+}
